fae9b330732f9944d3605266bd62a12ff50f84b8
Tiago Silva Rodrigues <tiago4r@gmail.com>
2024-01-09 16:02:39 +0000

    update docker compose

--- run_cluster.sh
+++ run_cluster.sh
@@ -6,14 +6,14 @@ docker network create hadoop_network
 # build docker image with image name hadoop-base:3.3.6
 docker build -t hadoop-base:3.3.6 -f Dockerfile-hadoop .
 # running image to container, -d to run it in daemon mode
-docker-compose -f docker-compose-hadoop.yml up -d
+docker compose -f docker-compose-hadoop.yml up -d
 
 # Run Airflow Cluster
 if [[ "$PWD" != "airflow" ]]; then
   cd airflow && ./run_airflow.sh && cd ..
 fi
 
-docker-compose -f docker-compose-airflow.yml up -d
+docker compose -f docker-compose-airflow.yml up -d
 
 # Run Spark Cluster
 if [[ "$PWD" != "spark" ]]; then
